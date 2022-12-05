0d0cef17827089a1abbe43082b361820777578fa
ManganiBiagio <biagiomangani@live.it>
2022-12-05 17:08:02 +0100

    add main content

---
+++ src/components/Themaincontent.vue
@@ -0,0 +1,38 @@
+<template >
+    <div class="img-box">
+        <img src="/imgs/jumbotron.jpg" class="img-fluid" alt="">
+    </div>
+    <div class="container">
+        <div class="row row-cols-6  g-4">
+            <div class="col" v-for="fumetti in fumettiList">
+                <div class="card">
+                    <img :src="fumetti.thumb" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{fumetti.series}}</h5>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        fumettiList:Array
+    }
+
+}
+</script>
+<style lang="scss" scoped>
+.img-box {
+    height: 400px;
+    overflow: hidden;
+}
+.card{
+    background-color: transparent;
+    border: unset;
+    color: white;
+}
+</style>
